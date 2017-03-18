51676b55e36b48e03152c9c7af78551f0bd23b36
UlrikaN <ulrika@ad.ssis.nu>
2017-03-18 23:22:02 +0100

    Lägger till

---
+++ script.js
@@ -0,0 +1,10 @@
+//alert("Hello World!");
+function myFunction(){
+	var element = index.getElementById('myElement');
+	element.innerHTML='My new text!';
+	element.style.fontSize = "100px";
+}
+
+//i html
+//<button onclick="myFunction">Click me!</button>
+//<p id="myElement">My text</p>
